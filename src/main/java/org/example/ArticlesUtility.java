413a852d6a9d5fc0f685f5dbf2a9631a1c06ac59
Slava <slawek.mymail@gmail.com>
2022-08-01 18:33:11 +0200

    changed Junit version to 4.13.2

--- src/main/java/org/example/ArticlesUtility.java
+++ src/main/java/org/example/ArticlesUtility.java
@@ -14,6 +14,7 @@ import java.util.Scanner;
 
 public class ArticlesUtility {
 
+    // test
 
     public static URL createURLTopHeadlines(String country, String category, String APIKey) throws MalformedURLException {
         String endpoint = "https://newsapi.org/v2/top-headlines";
